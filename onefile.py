acd66ef9be46f114384fafdf998a580ef23c55b2
emresvd <emresevindi99@gmail.com>
2023-01-30 22:35:41 +0300

    modified:   onefile.py	commit.commiter

--- onefile.py
+++ onefile.py
@@ -43,17 +43,11 @@ def import_(line):
     for module_name in module_s_name:
         if module_name.startswith('.'):
             module_name = module_name[1:]
-        # module_path = os.path.join(os.path.dirname(
-        #     start_file), module_name.replace(".", os.sep))
-        # module_path = os.path.abspath(module_path)
-        # module_path = os.path.normpath(module_path)
-        # module_path += '.py'
-
+            
         module_path = get_module_path(module_name)
 
         if os.path.isfile(module_path):
             project_modules.append(module_path)
-            print(os.path.dirname(module_path))
             putcode(module_path, line, module_name, from_import=True)
 
 
@@ -61,7 +55,7 @@ def from_(line):
     pass
 
 
-for i in range(3):
+while True:
     project_modules = []
 
     for line in code.splitlines():
@@ -69,7 +63,11 @@ for i in range(3):
             import_(line)
         if line.startswith('from'):
             from_(line)
+
     print(project_modules)
+
+    if not project_modules:
+        break
 
 
 # print(code)
