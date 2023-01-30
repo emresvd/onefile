dd41a4363be3a89235b7528ad110347af7d13a02
emresvd <emresevindi99@gmail.com>
2023-01-30 20:04:30 +0300

    modified:   onefile.py	commit.commiter

--- onefile.py
+++ onefile.py
@@ -1,88 +1,58 @@
-# This script converts multiple python files into one python file
-# there's no need
 import sys
-import os
 
-file_name = sys.argv[-1]
+start_file = sys.argv[-1]
 
-with open(file_name, "r", encoding="utf-8") as f:
+with open(start_file, 'r', encoding="utf-8") as f:
     code = f.read()
 
-module_files = []
-for i in code.splitlines():
-    i = i.strip()
-    if i.startswith("import"):
-        module_s = i.replace("import", "").strip()
 
-        if len(module_s.split(",")) > 1:
-            module_s = module_s.split(",")
-        else:
-            module_s = module_s.replace(".", os.sep)
-            module_files.append(module_s+".py")
+def strip(s):
+    l = []
+    for i in s:
+        l.append(i.strip())
+    return l
 
-    if i.startswith("from"):
-        from_module = i.replace("from", "").strip().split("import")[0].strip()
-        if from_module.startswith("."):
-            from_module = from_module.replace(".", "")
-        from_module = from_module.replace(".", os.sep)
-        module_files.append(from_module+".py")
 
-        try:
-            folder_import_file = i.replace("from", "").strip()
-            folder = folder_import_file.split("import")[0].strip()
-            file = folder_import_file.split("import")[1].strip()
-            module_files.append(os.path.join(folder, file)+".py")
-        except IndexError:
-            continue
+def import_(line):
+    module_s = map(line.replace("import", "").strip().split(","), strip)
+    print(module_s)
 
-project_modules = []
-for i in module_files:
-    if os.path.isfile(i):
-        if not i in project_modules:
-            project_modules.append(i)
 
-print(project_modules)
+def from_(line):
+    print(line)
 
-codes_of_all_modules = ""
-for i in project_modules:
-    with open(i, "r", encoding="utf-8") as f:
-        module_code = f.read()
 
-    module_code_without_name_main = ""
-    name_main = False
-    look_name_main_tab = False
+for line in code.splitlines():
+    if line.startswith('import'):
+        import_(line)
+    if line.startswith('from'):
+        from_(line)
+import sys
 
-    for j in module_code.splitlines():
-        if not bool(j):
-            continue
+start_file = sys.argv[-1]
 
-        tab = len(j.split(j.strip())[0])
+with open(start_file, 'r', encoding="utf-8") as f:
+    code = f.read()
 
-        if j.strip() == 'if __name__ == "__main__":' or j.strip() == 'if __name__=="__main__":' or j.strip() == "if __name__ == '__main__':" or j.strip() == "if __name__=='__main__':":
-            name_main = True
-            look_name_main_tab = True
-            continue
 
-        if look_name_main_tab:
-            name_main_tab = tab
-            look_name_main_tab = False
+def strip(s):
+    l = []
+    for i in s:
+        l.append(i.strip())
+    return l
 
-        try:
-            if tab < name_main_tab:
-                name_main = False
-            else:
-                name_main = True
-        except NameError:
-            pass
 
-        if not name_main:
-            module_code_without_name_main += j+"\n"
-        # if name_main:
-        #     print(j)
+def import_(line):
+    module_s = map(line.replace("import", "").strip().split(","), strip)
+    print(module_s)
 
-    codes_of_all_modules += module_code_without_name_main+"\n"
 
-#print(codes_of_all_modules)
+def from_(line):
+    print(line)
 
-with open(file_name.split(".")[0]+"_one.py", "w", encoding="utf-8") as f:
-    f.write(codes_of_all_modules+"\n"+code)
+
+for line in code.splitlines():
+    if line.startswith('import'):
+        import_(line)
+    if line.startswith('from'):
+        from_(line)
