d349285971d182efb179bc0447f5002d511d708e
emresvd <emresevindi99@gmail.com>
2023-01-30 20:05:25 +0300

    modified:   onefile.py	commit.commiter

--- onefile.py
+++ onefile.py
@@ -27,32 +27,3 @@ for line in code.splitlines():
         import_(line)
     if line.startswith('from'):
         from_(line)
-import sys
-
-start_file = sys.argv[-1]
-
-with open(start_file, 'r', encoding="utf-8") as f:
-    code = f.read()
-
-
-def strip(s):
-    l = []
-    for i in s:
-        l.append(i.strip())
-    return l
-
-
-def import_(line):
-    module_s = map(line.replace("import", "").strip().split(","), strip)
-    print(module_s)
-
-
-def from_(line):
-    print(line)
-
-
-for line in code.splitlines():
-    if line.startswith('import'):
-        import_(line)
-    if line.startswith('from'):
-        from_(line)
