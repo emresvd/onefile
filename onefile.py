71e4b15ea0cff8dfed64eb7a88b210fe79238990
emresvd <emresevindi99@gmail.com>
2023-01-30 21:28:36 +0300

    modified:   onefile.py	commit.commiter

--- onefile.py
+++ onefile.py
@@ -17,8 +17,15 @@ def putcode(module_path, line, module_name, from_import=False):
         module_code = f.read()
     code = code.replace(line, module_code)
     if from_import:
-        code = code.replace(f" {module_name}.", "")
-        code = code.replace(f"={module_name}.", "")
+        # if code != code.replace(f" {module_name}.", ""):
+        #     code = code.replace(f" {module_name}.", " ")
+        # if code != code.replace(f"={module_name}.", ""):
+        #     code = code.replace(f"={module_name}.", "=")
+        # if code != code.replace(f"({module_name}.", ""):
+        #     code = code.replace(f"({module_name}.", "(")
+        code = code.replace(f" {module_name}.", " ")
+        code = code.replace(f"={module_name}.", "=")
+        code = code.replace(f"({module_name}.", "(")
 
 
 def import_(line):
